2604597e595b02d3a2faaa81129905906f836f40
ermz <eramirez4369@gmail.com>
2021-09-01 23:35:49 -0400

    Fixed ropsten network connection issue

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,5 @@
 from brownie import options, accounts
 
 def main():
-    acct = accounts.load('local_deployment_account')
-    options.deploy(0, 0, 0, {"from": acct, "value": "10 ether"})
+    acct = accounts.load('0x6629985a1B15799c4330673458Fc899E545AeB4e')
+    options.deploy(0, 0, 0, {"from": acct})
