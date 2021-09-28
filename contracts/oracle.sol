e8e423aabf57a6a2fa28dc9c9d186a93dcf196a4
ermz <eramirez4369@gmail.com>
2021-09-27 23:58:29 -0400

    Made changes to oracle contract / Still needs work

--- contracts/oracle.sol
+++ contracts/oracle.sol
@@ -1,40 +1,32 @@
-// // SPDX-Licenses-Identifier: MIT
-// pragma solidity ^0.6.7;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
-// import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
-// contract APIConsumer is ChainlinkClient {
-//     using Chainlink for Chainlink.Request;
+contract PriceConsumerV3 {
 
-//     uint256 public volume;
+    AggregatorV3Interface internal priceFeed;
 
-//     address private oracle;
-//     bytes32 private jobId;
-//     uint256 private fee;
+    /**
+     * Network: Kovan
+     * Aggregator: ETH/USD
+     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     */
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+    }
 
-//     /** 
-//     * Network: Kovan
-//     * Fee: 0.1 LINK
-//     */
-
-//     constructor() public {
-//         setPublicChainlinkToken();
-//         oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
-//         jobId = "d5270d1c311941d0b08bead21fea7747";
-//         fee = 0.1 * 10 ** 18;
-//     }
-
-//     function requestsVolumeData() public returns (bytes32 requestId)
-//     {
-//         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-
-//         request.add("get", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD");
-
-//         request.add("path", "RAW.ETH.USD.VOLUME24HOUR");
-
-//         int timesAmount = 10**18;
-//         request.addInt("times", timesAmount);
-
-//         return sendChainlinkRequestTo(oracle, request, fee);
-//     }
-// }
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
